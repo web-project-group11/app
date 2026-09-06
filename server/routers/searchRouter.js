453e9fb24fe2a930df13348f6bd06c113a7ee599
Tommi <tommi.karsi@gmail.com>
2026-09-06 13:12:15 +0300

    now using nowplaying route

--- server/routers/searchRouter.js
+++ server/routers/searchRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { getMovies, getMovieData } from '../controllers/MovieController.js'
+import { getMovies, getMovieData, getNowPlayingMovies } from '../controllers/MovieController.js'
 
 const router = Router()
 
 router.get('/search', getMovies)
 router.get('/movie', getMovieData)
+router.get('/now-playing', getNowPlayingMovies)
 
 export default router
