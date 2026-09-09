f199b3e6d7520ad92f5722ffc38b447c55192f1c
Tommi <tommi.karsi@gmail.com>
2026-09-09 23:58:16 +0300

    added useStates for each use case

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -5,17 +5,29 @@ import axios from 'axios'
 export default function UserProvider({ children }) {
     const apiUrl = import.meta.env.VITE_API_URL
 
-    const userFromStorage = sessionStorage.getItem('user')
-    const [user, setUser] = useState(userFromStorage ? JSON.parse(userFromStorage) : { username: '', email: '', password: '', confirm_password: '' })
+    const userFromStorage = sessionStorage.getItem('authuser')
+    const [ signupUser, setSignupUser ] = useState({ username: '', email: '', password: '', confirm_password: '' })
+    const [ loginUser, setLoginUser ] = useState({ username: '', password: ''})
+    const [ authUser, setAuthUser ] = useState(userFromStorage ? JSON.parse(userFromStorage) : {id: '', username: '', password: ''})
 
     const signUp = async () => {
         const headers = { headers: { 'Content-Type': 'application/json' } }
-        await axios.post(`${apiUrl}/api/user/signup`, JSON.stringify({ user: user }), headers)
-        setUser({ username: '', email: '', password: '', confirm_password: '' })
+        await axios.post(`${apiUrl}/api/user/signup`, JSON.stringify({ user: signupUser }), headers)
+        setSignupUser({ username: '', email: '', password: '', confirm_password: '' })
     }
 
+    const logIn = async() => {
+        const headers = {headers: {'Content-Type': 'application/json'}}
+        const response = axios.post(`${apiUrl}/api/user/login`, JSON.stringify({ user: loginUser }), headers)
+        setLoginUser(response.data)
+        sessionStorage.setItem('authuser', JSON.stringify(response.data))
+    }
+
+    const logOut = async() => {
+        //sessionstorage tietojen poisto ja palautus kotisivulle??
+    }
     return (
-        <UserContext.Provider value={{ user, setUser, signUp }}>
+        <UserContext.Provider value={{ authUser, setAuthUser, signupUser, setSignupUser, loginUser, setLoginUser, signUp, logIn, logOut }}>
             {children}
         </UserContext.Provider>
     )
