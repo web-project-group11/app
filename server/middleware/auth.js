0a57d600912b0637b9ed4ea4fd37d4737c8a9d85
Tommi <tommi.karsi@gmail.com>
2026-09-10 04:52:20 +0300

    backend JWT authentication middleware done

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+import { ApiError } from '../helper/ApiError.js'
+
+const { verify } = jwt
+
+const auth = (req, _res, next) => {
+    const [scheme, token] = req.get('authorization')?.split(' ') || []
+
+    if(scheme !== 'Bearer' || !token){
+        return next(new ApiError('Authentication required', 401))
+    }
+    try{
+        req.user = verify(token, process.env.JWT_SECRET_KEY)
+    }catch{
+        return next(new ApiError('Invalid or expired token', 401))
+    }
+}
+
+export { auth }
