01ab02fc43a26604ab096648be7e9ba2418a3c7d
Tkarsikas <tommi.karsi@gmail.com>
2026-09-15 17:26:31 +0300

    add to my favorites button created

--- src/screens/MovieDetails.jsx
+++ src/screens/MovieDetails.jsx
@@ -3,11 +3,13 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Poster from "../components/Poster.jsx";
 import Reviews from "../components/MovieReviews.jsx";
+import { useUser } from "../context/useUser.jsx";
 
 const apiUrl = import.meta.env.VITE_API_URL
 
 function MovieDetails() {
   const { movieid } = useParams();
+  const { authUser } = useUser();
   
   const [movie, setMovie] = useState(null);
   const [reviews, setReviews] = useState([]);
@@ -42,6 +44,26 @@ function MovieDetails() {
 
   };
 
+  const handleAddFavorite = async () => {
+    try {
+      if (!authUser?.token) {
+        alert("You must be logged in to add favorites")
+        return
+      }
+      await axios.post(
+        `${apiUrl}/api/movie/favorites/${movieid}`,
+        {},
+        { 
+          headers: { 
+            Authorization: `Bearer ${authUser.token}` 
+          } }
+      )
+      alert("movie added to favorites")
+    }catch(error) {
+      alert(error.response?.data?.message || "Adding favorite failed")
+    }
+  }
+
   return (
     <div>
       <h3>Movie Details</h3>
@@ -50,8 +72,14 @@ function MovieDetails() {
       <p>Title: {movie?.title}</p>
       <p>Overview: {movie?.overview}</p>
       <p>Release Date: {movie?.release_date}</p>
-      <Reviews reviews={reviews} />  
-      <button>Add to MyFavorites</button> 
+      <Reviews reviews={reviews} />
+      {authUser.token ? (
+        <button type="button" onClick={handleAddFavorite}>
+          Add to MyFavorites
+        </button>
+      ):( 
+        <button type="button">Remove from favourites</button>
+       )} 
     </div>
   );
 }
