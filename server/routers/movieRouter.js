9289ab28364ce16f683d927f272482fd3ce25d1f
Tkarsikas <tommi.karsi@gmail.com>
2026-09-15 16:19:31 +0300

    added favorite route

--- server/routers/movieRouter.js
+++ server/routers/movieRouter.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { getMovieData, getNowPlayingMovies, getMovieReviews } from '../controllers/MovieController.js'
+import { myFavorite } from '../controllers/FavoriteController.js'
+import { auth } from '../middleware/auth.js'
 
 const router = Router()
 
 router.get('/', getMovieData)
 router.get('/now-playing', getNowPlayingMovies)
 router.get('/reviews/:movieId', getMovieReviews);
+router.post('/favorites/:movieId', auth, myFavorite)
 
 export default router
