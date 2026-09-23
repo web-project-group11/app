6893ae9a3667d15393272654cccf0b4346323d40
Aapo K <kartsoful@gmail.com>
2026-09-23 13:34:52 +0300

    Quick fix to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,100 +1,86 @@
-import { Link, useNavigate } from "react-router-dom"
-import { useUser } from "../../context/useUser.jsx"
-import { useState } from 'react'
-import SimpleSearch from "../SimpleSearch.jsx"
+import { Link, useNavigate } from "react-router-dom";
+import { useUser } from "../../context/useUser.jsx";
+import { useState } from "react";
+import SimpleSearch from "../SimpleSearch.jsx";
 
-import "./Header.css"
+import "./Header.css";
 
 function Header() {
+  const { authUser, logOut } = useUser();
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const { authUser, logOut } = useUser()
-    const navigate = useNavigate()
-    const [menuOpen, setMenuOpen] = useState(false)
+  const handleLogout = (e) => {
+    e.preventDefault();
+    setMenuOpen(false);
+    logOut();
+    navigate("/");
+  };
 
-    const handleLogout = (e) => {
-        e.preventDefault()
-        setMenuOpen(false)
-        logOut()
-        navigate('/')
-    }
+  const handleGoToProfile = (e) => {
+    e.preventDefault();
+    setMenuOpen(false);
+    navigate("/profile");
+  };
 
-    const handleGoToProfile = (e) => {
-        e.preventDefault()
-        setMenuOpen(false)
-        navigate('/profile')
-    }
+  const handleGoToUserFavorites = (e) => {
+    e.preventDefault();
+    setMenuOpen(false);
+    navigate(`/user/favorites/${authUser.username}`);
+  };
 
-    const handleGoToUserFavorites = (e) => {
-        e.preventDefault()
-        setMenuOpen(false)
-        navigate(`/user/favorites/${authUser.username}`)
-    }
+  return (
+    <header>
+      <Link className="brand-link" to="/">
+        Movie App
+      </Link>
 
+      <div>
+        <SimpleSearch />
+      </div>
+      <Link className="groups-link" to="/groups">
+        <button className="groups-button" type="button">
+          Groups
+        </button>
+      </Link>
 
-    return (
-        <header>
-            <Link className="brand-link" to='/'>Movie App</Link>
-            
-            <div>
-                <SimpleSearch />
-            </div>
-            <Link className="groups-link" to="/groups">
-                <button className="groups-button" type="button">Groups</button>
-            </Link>
+      {authUser.token ? (
+        <>
+          <Link className="profile-link" to="/profile">
+            <span>{authUser.username}</span>
+          </Link>
+        </>
+      ) : (
+        <Link className="login-link" to="/login">
+          <span>Login</span>
+        </Link>
+      )}
+      {authUser.token && (
+        <div className="menu-container">
+          <button
+            type="button"
+            className="hamburger"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
 
-            {authUser.token && (
-                <>
-                    <Link className="profile-link" to="/profile">
-                        <span>{authUser.username}</span>
-                    </Link>
-                </>
+          <div className="dropdown-menu">
+            {menuOpen && (
+              <>
+                <span className="menu-username">{authUser.username}</span>
+                <button onClick={handleGoToProfile}>Profile</button>
+                <button onClick={handleGoToUserFavorites}>Favorites</button>
+                <button onClick={handleLogout}>Log out</button>
+              </>
             )}
-            <div className="menu-container">
-
-                <button
-                    type="button"
-                    className="hamburger"
-                    onClick={() => setMenuOpen(!menuOpen)}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-
-                {menuOpen && (
-                    <div className="dropdown-menu">
-                        {authUser.token ? (
-                            <>
-                                <span className="menu-username">
-                                    {authUser.username}
-                                </span>
-
-                                <button onClick={handleGoToProfile}>
-                                    Profile
-                                </button>
-
-                                <button onClick={handleGoToUserFavorites}>
-                                    Favorites
-                                </button>
-
-                                <button onClick={handleLogout}>
-                                    Log out
-                                </button>
-                            </>
-                        ) : (
-                            <Link
-                                to="/login"
-                                onClick={() => setMenuOpen(false)}
-                            >
-                                Login
-                            </Link>
-                        )}
-                    </div>
-                )}
-
-            </div>
-        </header>
-    )
+          </div>
+        </div>
+      )}
+    </header>
+  );
 }
 
-export default Header
+export default Header;
